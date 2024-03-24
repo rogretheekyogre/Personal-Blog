a8577d0d46a71e98b373d9c1bf780abb12423022
Roger_Aponte <rogeraponte10@gmail.com>
2024-03-24 10:35:53 -0400

    Added updates3

--- blogs.js
+++ blogs.js
@@ -5,3 +5,16 @@ function myFunction() {
     element.classList.toggle("dark-mode")
 }
 localStorage.getItem("Username")
+localStorage.getItem("Title")
+localStorage.getItem("Content")
+
+var data =window.location.search;
+var params = new URLSearchParams(data);
+console.log(params);
+var Username = params.get("Username");
+var Title = params.get("Title");
+var Content = params.get("Content");
+
+var getData = "Username : "+Username+"
+
+document.getElementById("res") .innerHTML = data;
--- script.js
+++ script.js
@@ -3,15 +3,17 @@ window.location.href =""
 document.querySelector("button") 
 
 
-function myFunction() {
+function myFunction2() {
 var element = document.body;
 element.classList.toggle("dark-mode")
 }
 
 function myFunction(){
-alert("Please complete form!");
-
-
+    alert("Please complete form!");
+    result = Submit(question)
 }
+
+
 localStorage.setItem("Username", "RockyRoad");
 localStorage.setItem("Title" , "Blog Title");
+localStorage.setItem("Content" , "Blog 1");
